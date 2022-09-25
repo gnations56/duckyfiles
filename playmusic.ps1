68603817979be6b118035196dede4fb907e1c476
gnations56 <gnationsonyoutube@outlook.com>
2022-09-24 18:53:14 -0700

    add dropper

---
+++ playmusic.ps1
@@ -0,0 +1,15 @@
+function Start-Music
+{
+  $code = {
+    $player = New-Object -ComObject 'MediaPlayer.MediaPlayer'
+    $player.Open('https://github.com/gnations56/duckyfiles/raw/main/bestofme.mp3')
+    $player
+  }
+
+  $script:ps = [PowerShell]::Create()
+  $script:player = @($ps.AddScript($code).Invoke())[0]
+}
+
+Start-Music
+
+Read-Host
